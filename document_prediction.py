acc65ec143313246be4c841f17a292786b3439c1
Pratap <dreamweaver1231@gmail.com>
2016-12-20 21:33:02 +0530

    did some more changes

--- document_prediction.py
+++ document_prediction.py
@@ -6,34 +6,44 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 
-#get our data as an array from read_in()
-lines = sys.stdin.readlines()
-lines = json.loads(lines[0])
+def read_in():
+    lines = sys.stdin.readlines()
+    # Since our input would only be having one line, parse our JSON data from that
+    return json.loads(lines[0])
 
-#create a numpy array
-input_data = np.array(lines)
+def main():
+    #get our data as an array from read_in()
+    #lines = sys.stdin.readlines()
+    #lines = json.loads(lines[0])
+
+    #create a numpy array
+    input_data = np.array(read_in())
 
 
-stop_Words = stopwords.words('english')
-pipeline = Pipeline([
-    ('bow', CountVectorizer(stop_words=stop_Words)),
-    ('tfidf', TfidfTransformer()),
-    ('classifier', MultinomialNB())
-])
+    stop_Words = stopwords.words('english')
+    pipeline = Pipeline([
+        ('bow', CountVectorizer(stop_words=stop_Words)),
+        ('tfidf', TfidfTransformer()),
+        ('classifier', MultinomialNB())
+    ])
 
-search_criteria = input_data
+    search_criteria = input_data
 
-stop_Words = stopwords.words('english')
-df = pd.read_csv('document-data.csv')
+    stop_Words = stopwords.words('english')
+    df = pd.read_csv('document-data.csv')
 
-x_data = df['description']
-y_data = df['id']
+    x_data = df['description']
+    y_data = df['id']
 
-model = pipeline.fit(x_data,y_data)
-prediction_prob = model.predict_proba(search_criteria)
-df1 = pd.DataFrame(prediction_prob[0], columns=['probability'])
-result = pd.concat([df, df1], axis=1)
-result.sort_values(by='probability', ascending=False, inplace=True)
+    model = pipeline.fit(x_data,y_data)
+    prediction_prob = model.predict_proba(search_criteria)
+    df1 = pd.DataFrame(prediction_prob[0], columns=['probability'])
+    result = pd.concat([df, df1], axis=1)
+    result.sort_values(by='probability', ascending=False, inplace=True)
 
-print(result.head(5))
-sys.stdout.flush()
+    print(result.head(5))
+    sys.stdout.flush()
+
+# Start process
+if __name__ == '__main__':
+    main()
