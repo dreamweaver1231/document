7b5a0f7b42f456c35b9390a387aa91fbb7e05028
Pratap <dreamweaver1231@gmail.com>
2017-01-16 12:34:14 +0530

    remove nltk

--- Document_Prediction_Sample_1.py
+++ Document_Prediction_Sample_1.py
@@ -1,12 +1,10 @@
 import pandas as pd
 
-from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 
-stop_Words = stopwords.words('english')
 pipeline = Pipeline([
     ('bow', CountVectorizer(stop_words=stop_Words)),
     ('tfidf', TfidfTransformer()),
@@ -14,8 +12,6 @@ pipeline = Pipeline([
 ])
 
 search_criteria = ['Print banded betina btm']
-
-stop_Words = stopwords.words('english')
 df = pd.read_csv('document-data.csv')
 
 x_data = df['description']
